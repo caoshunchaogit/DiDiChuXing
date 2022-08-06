c932dc79094f72220f8ac9e0b3983d3075a29c7a
992995879@qq.com <992995879@qq.com>
2022-08-06 13:16:50 +0800

    搭建验证码服务

--- server-verificationcode/src/main/java/com/csc/ServerVerificationcodeApplication.java
+++ server-verificationcode/src/main/java/com/csc/ServerVerificationcodeApplication.java
@@ -1,19 +1,15 @@
-package com.csc.apipassenger;
+package com.csc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-/**
- * @program: 这是测试类
- * @description:
- * @author: 曹顺超
- * @create: 2022-08-06 10:27
- **/
 @SpringBootApplication
 @EnableDiscoveryClient
-public class ApiPassengerApplication {
+public class ServerVerificationcodeApplication {
+
     public static void main(String[] args) {
-        SpringApplication.run(ApiPassengerApplication.class);
+        SpringApplication.run(ServerVerificationcodeApplication.class, args);
     }
+
 }
